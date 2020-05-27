5940d06c9944877693da373ab84876b3beedee17
Your Name <amirfathi888@outlook.com>
2020-05-27 22:12:07 +0430

    solve travis error

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,8 +1,6 @@
 from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from rest_framework import status
 
 from core.models import Tag
 
